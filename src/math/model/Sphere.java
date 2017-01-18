3cf54195293067797824cae636334b587cdec08d
AarooneyFarr <Aarooney.Farr@gmail.com>
2017-01-18 09:55:44 -0700

    add

--- src/math/model/Sphere.java
+++ src/math/model/Sphere.java
@@ -12,6 +12,10 @@ public class Sphere extends JPanel
 	{
 		private MathController baseController;
 		private JComboBox<JPanel> sphereSolverSelector;
+		private JTextField radText;
+		private JTextField diaText;
+		private JLabel radLabel;
+		private JLabel diaLabel;
 
 		
 		 public Sphere(MathController baseController)
@@ -21,7 +25,11 @@ public class Sphere extends JPanel
 
 			this.baseController = baseController;
 			sphereSolverSelector = new JComboBox<JPanel>(baseController.getMathDex());
-
+			radText = new JTextField();
+			diaText = new JTextField();
+			radLabel = new JLabel("Radius: ");
+			diaLabel = new JLabel("Diameter: ");
+			
 			
 			setupPanel();
 			setupLayout();
@@ -39,13 +47,21 @@ public class Sphere extends JPanel
 		{
 			setLayout(null);
 			this.add(sphereSolverSelector);
-
+			this.add(radText);
+			this.add(diaText);
+			this.add(radLabel);
+			this.add(diaLabel);
+			
+			
 		}
 		
 		public void setupLayout()
 		{
 			sphereSolverSelector.setBounds(6,6,159,27);
-
+			radText.setBounds(114, 64, 78, 27);
+			diaText.setBounds(306, 64, 78, 27);
+			radLabel.setBounds(46, 64, 56, 27);
+			diaLabel.setBounds(235, 64, 65, 27);
 		}
 		
 		public void setupListeners()
